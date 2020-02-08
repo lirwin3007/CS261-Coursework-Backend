21a86ccd5c25cda863cd9ed069f0952a9a7241d9
JDJFisher <jordanfisher2096@gmail.com>
2020-02-08 15:08:26 +0000

    🚧 Started work on derivative endpoints

--- backend/blueprints/derivative_blueprint.py
+++ backend/blueprints/derivative_blueprint.py
@@ -1,9 +1,9 @@
 # Third party imports
-from flask import Blueprint
+from flask import Blueprint, abort, jsonify, request
 
 # Local application imports
-# You can import database models with 'from backend.derivatex_models import Derivative, User, Action'
 from backend.managers import derivative_management
+from backend.derivatex_models import Derivative
 
 # Instantiate new blueprint
 DerivativeBlueprint = Blueprint('derivativeManagement',
@@ -12,6 +12,105 @@ DerivativeBlueprint = Blueprint('derivativeManagement',
 
 
 # Routes
-@DerivativeBlueprint.route('/example-route/<exampleParam>')
-def exampleRoute(exampleParam):
-    return derivative_management.exampleFunction(exampleParam)
+@DerivativeBlueprint.route('/get-derivative/<derivativeId>')
+def getDerivative(derivativeId):
+    # Get derivative from database
+    derivative = derivative_management.getDerivative(derivativeId)
+    # Make response
+    return derivative.as_dict() if derivative is not None else abort(404)
+
+
+@DerivativeBlueprint.route('/add-derivative', methods=['POST'])
+def addDerivative():
+    # Verify request
+    if not request.data or not request.is_json:
+        return abort(400)
+
+    # Extract json body from request
+    body = request.get_json()
+
+    # Validate the json body
+    # if invalid body:
+    #     return flask.abort(400)
+
+    # Extract user_id and create derivative object
+    user_id = body.get('user_id')
+    derivative = Derivative(**body.get('derivative'))
+
+    # Add derivative to database
+    added = derivative_management.addDerivative(derivative, user_id)
+
+    # Make response
+    return jsonify(id=derivative.id) if added else abort(400)
+
+
+@DerivativeBlueprint.route('/update-derivative/<derivativeId>', methods=['POST'])
+def updateDerivative(derivativeId):
+    # Verify request
+    if not request.data or not request.is_json:
+        return abort(400)
+
+    # Extract json body from request
+    body = request.get_json()
+
+    # Validate the json body
+    # if invalid body:
+    #     return flask.abort(400)
+
+    # Extract user_id and updates
+    user_id = body.get('user_id')
+    updates = body.get('updates')
+
+    # Retreive the specified derivative
+    derivative = derivative_management.getDerivative(derivativeId)
+    if derivative is None:
+        return abort(404)
+
+    # Update the derivative
+    updated = derivative_management.updateDerivative(derivative, user_id, updates)
+
+    # Make response
+    return jsonify(updates) if updated else abort(400)
+
+
+@DerivativeBlueprint.route('/delete-derivative/<derivativeId>', methods=['POST'])
+def deleteDerivative(derivativeId):
+    # Verify request
+    if not request.data or not request.is_json:
+        return abort(400)
+
+    # Obtatin user_id
+    body = request.get_json()
+    user_id = body.get('user_id')
+    if user_id is None:
+        return abort(400)
+
+    # Retreive the specified derivative
+    derivative = derivative_management.getDerivative(derivativeId)
+    if derivative is None:
+        return abort(404)
+
+    # Delete the derivative
+    derivative_management.deleteDerivative(derivative, user_id)
+
+    # Return a 204, no content
+    return '', 204
+
+
+@DerivativeBlueprint.route('/index-derivatives')
+def indexDerivatives():
+    # Retreive form
+    form_dict = request.form.to_dict()
+
+    # Determine filters
+    filters = []
+
+    # Determine page parameters
+    page_size = form_dict.get('page_size') or 15
+    page_number = form_dict.get('page_number') or 0
+
+    # Index derivatives
+    derivatives, page_count = derivative_management.indexDerivatives(filters, page_size, page_number)
+
+    # Make response
+    return jsonify(page_count=page_count, derivatives=[d.id for d in derivatives])
--- backend/managers/derivative_management.py
+++ backend/managers/derivative_management.py
@@ -1,6 +1,91 @@
-import json
-# You can use 'from flask import abort' and then 'abort(http_code)' to abort requests
+# Local application imports
+from backend.derivatex_models import Derivative, Action, ActionType
+from backend.db import db
 
 
-def exampleFunction(exampleParam):
-    return json.dumps(int(exampleParam) + 1)
+def getDerivative(derivativeId):
+    # Query database for the derivative
+    return Derivative.query.filter_by(deleted=False, id=derivativeId).first()
+
+
+def addDerivative(derivative, user_id):
+    # Validate the derivative
+    # if invalid derivative:
+    #     return False
+
+    # Add the derivative and corrosponding user action to the database
+    action = Action(derivative_id=derivative.id, user_id=user_id, type=ActionType.ADD)
+    db.session.add(action)
+    db.session.add(derivative)
+    db.session.commit()
+
+    # Return success
+    return True
+
+
+def updateDerivative(derivative, user_id, updates):
+    # Apply and log all updates to the derivative
+    update_log = []
+    for attribute, new_value in updates.items():
+        old_value = getattr(derivative, attribute)
+
+        # Perform update
+        setattr(derivative, attribute, new_value)
+
+        # Log update
+        update_log.append({
+            "attribute": attribute,
+            "old_value": old_value,
+            "new_value": new_value
+        })
+
+    # Validate the updated derivative
+    # if invalid derivative:
+    #     return False
+
+    # Register the derivative updates and corrosponding user action to the database
+    action = Action(derivative_id=derivative.id, user_id=user_id, type=ActionType.UPDATE, update_info=update_log)
+    db.session.add(action)
+    db.session.add(derivative)
+    db.session.commit()
+
+    # Replace the update request with the update log
+    updates.clear()
+    updates['updates'] = update_log
+
+    # Return success
+    return True
+
+
+def deleteDerivative(derivative, user_id):
+    # Mark the derivative as deleted
+    derivative.deleted = True
+
+    # Register the user deleting the derivative
+    action = Action(derivative_id=derivative.id, user_id=user_id, type=ActionType.DELETE)
+    db.session.add(derivative)
+    db.session.add(action)
+    db.session.commit()
+
+
+def indexDerivatives(filters, page_size, page_number):
+    # Create base query
+    query = Derivative.query
+
+    # # Apply all query filters
+    # for filter in filters:
+    #     query = query.filter(filter)
+
+    # # Order the query
+    # if order_key is not None:
+    #     query = query.order_by(order_key)
+
+    # Paginate the query
+    page_count = query.count() // page_size + 1
+    query = query.limit(page_size).offset(page_size * page_number)
+
+    # Execute query
+    derivatives = query.all()
+
+    # Return derivatives and page count
+    return derivatives, page_count
