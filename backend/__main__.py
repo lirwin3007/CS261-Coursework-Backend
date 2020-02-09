09adc5acf51df9522f63a79c3ff7cd73d6bc6ad8
JDJFisher <jordanfisher2096@gmail.com>
2020-02-09 09:46:14 +0000

    🚧 Reworking app config, updated deprecated SQLAlchemy pool config values

--- backend/__main__.py
+++ backend/__main__.py
@@ -1,4 +1,4 @@
 from .app import Application
 
 if __name__ == '__main__':
-    Application.run()
+    Application.runDevServer()
--- backend/app.py
+++ backend/app.py
@@ -10,32 +10,54 @@ from backend.blueprints.report_blueprint import ReportBlueprint
 class Application:
 
     @staticmethod
-    def setup():
+    def setupApp(configObj):
+        # Create flask app
         app = Flask(__name__)
         app.app_context().push()
+        app.config.from_object(configObj)
 
-        app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://derivatex_backend:qwerty123@localhost/derivatex'
-        app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-        app.config['SQLALCHEMY_BINDS'] = {'external':
-                                          'mysql+mysqlconnector://derivatex_backend:qwerty123@localhost/external'}
-        app.config['SQLALCHEMY_POOL_SIZE'] = 100
-        app.config['SQLALCHEMY_POOL_RECYCLE'] = 1
-
+        # Bind SQLAlchemy database engine to flask app
         db.init_app(app)
+        # Create all schemas defined by ORM models
         db.create_all()
 
+        # Register route blueprints
         app.register_blueprint(DerivativeBlueprint)
         app.register_blueprint(UserBlueprint)
         app.register_blueprint(ActionBlueprint)
         app.register_blueprint(ReportBlueprint)
+
+        # Return app reference
         return app
 
     @staticmethod
-    def run():
-        app = Application.setup()
+    def runDevServer():
+        # Setup app
+        app = Application.setupApp(Config)
+        # Run flask development server
         app.run()
 
     @staticmethod
-    def get_app():
-        app = Application.setup()
-        return app
+    def getProductionApp():
+        # Setup and return app
+        return Application.setupApp(Config)
+
+    @staticmethod
+    def getTestApp():
+        # Setup and return app
+        return Application.setupApp(Config)
+
+
+class Config:
+    DEBUG = False
+    TESTING = False
+    JSON_SORT_KEYS = True
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://derivatex_backend:qwerty123@localhost/derivatex'
+    SQLALCHEMY_ECHO = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_BINDS = {
+        'external': 'mysql+mysqlconnector://derivatex_backend:qwerty123@localhost/external'
+    }
+    SQLALCHEMY_ENGINE_OPTIONS = {
+        'pool_size': 100, 'pool_recycle': 1, 'pool_pre_ping': True
+    }
