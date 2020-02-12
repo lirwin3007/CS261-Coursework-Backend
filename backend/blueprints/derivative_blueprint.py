72304b82b7042a4c27f8147596655c68f2eed807
JDJFisher <jordanfisher2096@gmail.com>
2020-02-12 22:22:22 +0000

    🚧 Started work on indexDerivatives filtering

--- backend/blueprints/derivative_blueprint.py
+++ backend/blueprints/derivative_blueprint.py
@@ -55,6 +55,30 @@ def addDerivative():
     return jsonify(id=derivative.id)
 
 
+@DerivativeBlueprint.route('/delete-derivative/<derivative_id>', methods=['DELETE'])
+def deleteDerivative(derivative_id):
+    # Verify request
+    if not request.data or not request.is_json:
+        return abort(400)
+
+    # Obtatin user_id
+    body = request.get_json()
+    user_id = body.get('user_id')
+    if user_id is None:
+        return abort(400)
+
+    # Retreive the specified derivative
+    derivative = derivative_management.getDerivative(derivative_id)
+    if derivative is None:
+        return abort(404)
+
+    # Delete the derivative
+    derivative_management.deleteDerivative(derivative, user_id)
+
+    # Return a 204, no content
+    return '', 204
+
+
 @DerivativeBlueprint.route('/update-derivative/<derivative_id>', methods=['POST'])
 def updateDerivative(derivative_id):
     # Verify request
@@ -91,44 +115,17 @@ def updateDerivative(derivative_id):
     return jsonify(update_log=update_log)
 
 
-@DerivativeBlueprint.route('/delete-derivative/<derivative_id>', methods=['DELETE'])
-def deleteDerivative(derivative_id):
-    # Verify request
-    if not request.data or not request.is_json:
-        return abort(400)
-
-    # Obtatin user_id
-    body = request.get_json()
-    user_id = body.get('user_id')
-    if user_id is None:
-        return abort(400)
-
-    # Retreive the specified derivative
-    derivative = derivative_management.getDerivative(derivative_id)
-    if derivative is None:
-        return abort(404)
-
-    # Delete the derivative
-    derivative_management.deleteDerivative(derivative, user_id)
-
-    # Return a 204, no content
-    return '', 204
-
-
 @DerivativeBlueprint.route('/index-derivatives')
 def indexDerivatives():
-    # Retreive form
-    form_dict = request.form.to_dict()
-
-    # Determine filters
-    filters = []
+    # Get request
+    body = request.get_json() if request.data and request.is_json else {}
 
     # Determine page parameters
-    page_size = form_dict.get('page_size') or 15
-    page_number = form_dict.get('page_number') or 0
+    page_size = body.get('page_size') or 15
+    page_number = body.get('page_number') or 0
 
     # Index derivatives
-    derivatives, page_count = derivative_management.indexDerivatives(filters, page_size, page_number)
+    derivatives, page_count = derivative_management.indexDerivatives(body, page_size, page_number)
 
     # Make response
     return jsonify(page_count=page_count, derivatives=[d.id for d in derivatives])
--- backend/managers/derivative_management.py
+++ backend/managers/derivative_management.py
@@ -1,3 +1,10 @@
+# Standard library imports
+import datetime
+import operator
+
+# Third party imports
+from sqlalchemy import asc, desc
+
 # Local application imports
 from backend.derivatex_models import Derivative, Action, ActionType
 from backend.db import db
@@ -17,6 +24,21 @@ def addDerivative(derivative, user_id):
     action = Action(derivative_id=derivative.id, user_id=user_id, type=ActionType.ADD)
     db.session.add(action)
     db.session.flush()
+
+
+def deleteDerivative(derivative, user_id):
+    # The derivative is already flagged as deleted, return
+    if derivative.deleted:
+        return
+
+    # Mark the derivative as deleted
+    derivative.deleted = True
+
+    # Register the user deleting the derivative
+    action = Action(derivative_id=derivative.id, user_id=user_id, type=ActionType.DELETE)
+    db.session.add(derivative)
+    db.session.add(action)
+    db.session.commit()
 
 
 def updateDerivative(derivative, user_id, updates):
@@ -52,39 +74,72 @@ def updateDerivative(derivative, user_id, updates):
     return update_log
 
 
-def deleteDerivative(derivative, user_id):
-    # The derivative is already flagged as deleted, return
-    if derivative.deleted:
-        return
-
-    # Mark the derivative as deleted
-    derivative.deleted = True
-
-    # Register the user deleting the derivative
-    action = Action(derivative_id=derivative.id, user_id=user_id, type=ActionType.DELETE)
-    db.session.add(derivative)
-    db.session.add(action)
-    db.session.commit()
-
-
-def indexDerivatives(filters, page_size, page_number):
+def indexDerivatives(filter_dict, page_size, page_number):  # noqa: C901
     # Create base query
     query = Derivative.query
 
-    # # Apply all query filters
-    # for filter in filters:
-    #     query = query.filter(filter)
+    # Initialise filter sets
+    pre_filters = set()
+    post_filters = set()
 
-    # # Order the query
-    # if order_key is not None:
-    #     query = query.order_by(order_key)
+    # Determine filters
+    for filter_name, value in filter_dict.items():
+        if filter_name.startswith('min_'):
+            filter_name = filter_name[4:]
+            op = operator.ge
+        elif filter_name.startswith('max_'):
+            filter_name = filter_name[4:]
+            op = operator.le
+        else:
+            op = operator.eq
 
-    # Paginate the query
-    page_count = query.count() // page_size + 1
-    query = query.limit(page_size).offset(page_size * page_number)
+        if hasattr(Derivative, filter_name):
+            if filter_name in Derivative.__table__.columns:
+                pre_filters.add(op(Derivative.__table__.columns[filter_name], value))
+            else:
+                post_filters.add(lambda d: op(getattr(d, filter_name), value))
 
-    # Execute query
-    derivatives = query.all()
+    # Apply all pre-filters to the base query
+    for f in pre_filters:
+        query = query.filter(f)
+
+    # Determine derivative ordering key
+    order_key = filter_dict.get('order_key')
+    reverse_order = filter_dict.get('reverse_order') or False
+
+    # Order the query
+    if order_key is not None and order_key in Derivative.__table__.columns:
+        fun = desc if reverse_order else asc
+        query = query.order_by(fun(order_key))
+
+    # Calculate derivative offset
+    offset = page_size * page_number
+
+    if post_filters or order_key == 'notional_value':
+        # Execute sql query
+        derivatives = query.all()
+
+        # Sort derivatives
+        if order_key == 'notional_value':
+            derivatives.sort(key=lambda d: d.notional_value, reverse=reverse_order)
+
+        # Apply all post-query filters to derivatives
+        for f in post_filters:
+            derivatives = [d for d in derivatives if f(d)]
+
+        # Paginate derivatives
+        derivative_count = len(derivatives)
+        derivatives = derivatives[offset:offset + page_size]
+    else:
+        # Paginate query
+        derivative_count = query.count()
+        query = query.limit(page_size).offset(offset)
+
+        # Execute sql query
+        derivatives = query.all()
+
+    # Determine page count
+    page_count = derivative_count // page_size + 1
 
     # Return derivatives and page count
     return derivatives, page_count
