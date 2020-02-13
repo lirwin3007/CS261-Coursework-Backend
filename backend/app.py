e1fb9e3e19d54ffffa85783f4cdc4721c58db1ad
05hazza06 <43830657+05hazza06@users.noreply.github.com>
2020-02-13 19:32:24 +0000

    Update backend/app.py
    Fix lint error on apscheduler job
    
    Co-Authored-By: Louis Irwin <coding@louisirwin.co.uk>

--- backend/app.py
+++ backend/app.py
@@ -26,7 +26,7 @@ class Application:
         scheduler = APScheduler()
         scheduler.init_app(app)
         scheduler.start()
-        app.apscheduler.add_job(func=report_management.generateReports, trigger='cron', hour='23', minute='59', id='j1')
+        app.apscheduler.add_job(func=report_management.generateReports, trigger='cron', hour='23', minute='59', id='j1') # pylint: disable=E1101
 
         # Bind SQLAlchemy database engine to flask app
         db.init_app(app)
