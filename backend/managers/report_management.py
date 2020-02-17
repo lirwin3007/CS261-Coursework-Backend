080b972a377d2b0c245b50665cc742c3e198a991
Harry Roberts <harry.roberts@gmx.com>
2020-02-17 20:25:57 +0000

    :sparkles: Implemented index reports including pagination

--- backend/managers/report_management.py
+++ backend/managers/report_management.py
@@ -2,11 +2,32 @@
 from flask import send_file
 
 # Local application imports
-# from backend.derivatex_models import Report
+from backend.derivatex_models import Report, Derivative
+from backend.db import db
+from backend.util import clamp
 
 
-def indexReports(date_from, date_to):
-    return {"report_ids": [1, 2, 3]}
+def indexReports(body, page_size, page_number, date_from, date_to):  # noqa: C901
+    # Create base query
+    query = Report.query
+
+    # Apply filters to query
+    query = query.filter(operator.ge(Report.__table__.collumns[target_date], date_from))
+    query = query.filter(operator.le(Report.__table__.collumns[target_date], date_to))
+
+    # Order query
+    query = query.order_by(asc(target_date))
+
+    # Determine page count
+    page_count = query.count() // page_size + 1
+    # Calculate index offset
+    offset = page_size * (clamp(page_number, 1, page_count) - 1)
+    # Paginate query
+    query = query.limit(page_size).offset(offset)
+    # Execute SQL query
+    reports = query.all()
+
+    return reports, page_count
 
 
 def getReport(report_id):
