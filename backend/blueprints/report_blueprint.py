85c6774054c16db0ff671fb1f19444a7cff1921c
Harry Roberts <harry.roberts@gmx.com>
2020-02-17 18:32:49 +0000

    :sparkles: Improved report blueprint to include aborts, pagination, non-url arguments etc

--- backend/blueprints/report_blueprint.py
+++ backend/blueprints/report_blueprint.py
@@ -1,5 +1,5 @@
 # Third party imports
-from flask import Blueprint
+from flask import Blueprint, abort, jsonify, request
 
 # Local application imports
 from backend.managers import report_management
@@ -11,26 +11,44 @@ ReportBlueprint = Blueprint('reportManagement',
 
 
 # Routes
-@ReportBlueprint.route('/index-reports/<date_from>/<date_to>')
-def indexReports(date_from, date_to):
-    # Get ids from database
-    reports = report_management.indexReports(date_from, date_to)
+@ReportBlueprint.route('/index-reports')
+def indexReports():
+    # Get request
+    body = request.get_json() if request.data and request.is_json else {}
+
+    # Retrieve input data
+    date_from = request.args.get('date_from', default=None, type=String)
+    date_to = request.args.get('date_to', default=None, type=String)
+
+    # Determine page parameters
+    page_size = max(body.get('page_size') or 15, 1)
+    page_number = request.args.get('page_number', default=0, type=int)
+
+    # Index reports
+    reports, page_count = report_management.indexReports(body, page_size, page_number, date_from, date_to)
+
     # Make response
-    return reports
+    return jsonify(page_count=page_count, reports=[r.id for r in reports])
 
 
 @ReportBlueprint.route('/get-report/<report_id>')
 def getReport(report_id):
     # Get report from file system and info from db
     report = report_management.getReport(report_id)
+    if report is None
+        return abort(404)
+
     # Make response
-    return report
+    return jsonify(report=report)
 
 
 @ReportBlueprint.route('/download-report/<format>/<report_id>')
 def downloadReport(format, report_id):
-    if format == "CSV":
+    if format == 'CSV':
         CSV_file = report_management.downloadCSV(report_id)
-        return CSV_file
+        if CSV_file is None:
+            return abort(404)
+
     PDF_file = report_management.downloadPDF(report_id)
-    return PDF_file
+    if PDF_file is None:
+        return abort(404)
