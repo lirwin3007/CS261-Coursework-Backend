613ab9e135070d7533d0e079b7f47b9e6e91ef41
Neeraj Hari Krishnan <nhkrishnan4@gmail.com>
2020-02-18 17:13:05 +0000

    🚨 Removed redundant code

--- tests/test_user_account_control.py
+++ tests/test_user_account_control.py
@@ -1,8 +1,7 @@
 # Local application imports
-from tests import context # noqa # pylint: disable=unused-import
-from backend.derivatex_models import User
 from backend.managers import user_management
 from backend.db import db
+
 
 def testGetUser(dummy_user):
     # Add dummy user to database session
