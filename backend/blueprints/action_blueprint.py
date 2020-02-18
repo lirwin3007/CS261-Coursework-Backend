fc55b6114bfff69003966217a138fd2e0aa70b42
JDJFisher <jordanfisher2096@gmail.com>
2020-02-18 11:01:20 +0000

    🥅 Updated action blueprint validation

--- backend/blueprints/action_blueprint.py
+++ backend/blueprints/action_blueprint.py
@@ -3,6 +3,7 @@ from flask import Blueprint, abort, jsonify
 
 # Local application imports
 from backend.managers import action_management
+from backend.managers import user_management
 
 # Instantiate new blueprint
 ActionBlueprint = Blueprint('actionManagement',
@@ -15,8 +16,10 @@ ActionBlueprint = Blueprint('actionManagement',
 def getAction(action_id):
     # Get action from database
     action = action_management.getAction(action_id)
+
+    # Verify action exists
     if action is None:
-        abort(404)
+        return abort(404, 'action id {} does not exist'.format(action_id))
 
     # Make response
     return jsonify(action=action)
@@ -24,8 +27,13 @@ def getAction(action_id):
 
 @ActionBlueprint.route('/get-user-actions/<user_id>')
 def getUserActions(user_id):
+    # Validate user id
+    if user_management.getUser(user_id) is None:
+        return abort(404, 'user id {} does not exist'.format(user_id))
+
     # Get user actions from database
     actions = action_management.getUserActions(user_id)
+
     # Make response
     return jsonify(actions=[a.id for a in actions])
 
@@ -34,5 +42,6 @@ def getUserActions(user_id):
 def getRecentActions(count):
     # Get recent actions from database
     actions = action_management.getRecentActions(count)
+
     # Make response
     return jsonify(actions=[a.id for a in actions])
