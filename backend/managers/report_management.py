473784651e4c475d657ad788947be5d7999b0191
Harry Roberts <harry.roberts@gmx.com>
2020-02-19 22:31:07 +0000

    :sparkles: Implemented report generation

--- backend/managers/report_management.py
+++ backend/managers/report_management.py
@@ -1,7 +1,14 @@
 # Standard library imports
+from datetime import datetime
+import operator
+import csv
+
+# Third party imports
 from flask import send_file
+from sqlalchemy import func
 
 # Local application imports
+from backend.managers import derivative_management
 from backend.derivatex_models import Report, Derivative
 from backend.db import db
 from backend.util import clamp
@@ -54,8 +61,39 @@ def createPDF(report_id):
 
 
 def generateReports():
-    # Select dates from Derivative table not reported
-    # Create new entry in report table with stats
-    # Create new CSV file in static with id
-    # Populate CSV with derivatives
+    report_dates = db.session.query(Derivative.date_of_trade.distinct()).filter_by(reported=False).all()
+
+    for target_date in report_dates:
+        # Get next report id or initialise as 0
+        report_id = db.session.query(func.max(Report.id)).scalar()
+        if report_id is None:
+            report_id = 0
+        else
+            report_id += 1
+
+        # Make CSV and open for writing
+        with open('static/reports/' + report_id + '.csv', 'w', newline=' ') as file:
+            writer = csv.writer(file)
+            # Get derivatives for target_date that arent deleted
+            derivatives = Derivative.query.filter_by(date_of_trade=target_date).filter_by(deleted=False).order_by(asc(id)).all()
+            for d in derivatives:
+                # Write each derivative to file and set reported attribute
+                writer.writerow([d.id, d.code, d.buying_party, d.selling_party, d.asset, d.quantity, d.strike_price, d.currency_code, d.date_of_trade, d.maturity_date])
+                derivative_management.getDerivative(d.id).reported = True
+
+        # Get creation date
+        creation_date = datetime.date(datetime.now())
+
+        # Get next version of report or initialise as 0
+        version = db.session.query(func.max(Report.version)).filter_by(target_date=target_date).scalar()
+        if version is None:
+            version = 0
+        else
+            version += 1
+
+        report = Report(report_id, target_date, creation_date, version)
+        db.add(report)
+        db.flush()
+        db.session.commit()
+
     return True
