c5bc2540fed3e4a0e258bcba3dcb3fbc9f94d219
Louis Irwin <coding@louisirwin.co.uk>
2020-02-20 00:41:22 +0000

    🗃 Added learned behaviour database models

--- backend/derivatex_models.py
+++ backend/derivatex_models.py
@@ -1,6 +1,7 @@
 # Standard library imports
 from datetime import datetime
 import enum
+import statistics
 
 # Local application imports
 from backend.db import db
@@ -89,3 +90,169 @@ class Report(db.Model):
 
     def __str__(self):
         return '<Report : {}>'.format(self.id)
+
+
+class Features(str, enum.Enum):
+    BUYING_PARTY = 'BUYING_PARTY'
+    SELLING_PARTY = 'SELLING_PARTY'
+    ASSET = 'ASSET'
+    QUANTITY = 'QUANTITY'
+    STRIKE_PRICE = 'STRIKE_PRICE'
+
+
+class Label(str, enum.Enum):
+    ERRONEOUS = 'ERRONEOUS'
+    VALID = 'VALID'
+
+
+class DecisionTreeNode(db.Model):
+    id = db.Column(db.BigInteger, primary_key=True)
+    parent_id = db.Column(db.BigInteger, db.ForeignKey('decision_tree_node.id'))
+    true_node_id = db.Column(db.BigInteger, db.ForeignKey('decision_tree_node.id'))
+    false_node_id = db.Column(db.BigInteger, db.ForeignKey('decision_tree_node.id'))
+    feature = db.Column(db.Enum(Features))
+    criteria = db.Column(db.Text)
+    label = db.Column(db.Enum(Label))
+
+    def calculateBuyingPartyFeature(self, item):
+        """ Calculate the buying party feature of a given item.
+
+        Args:
+            item (Derivative): The derivative to get the feature for
+
+        Returns:
+            Boolean: The value of the feature for the given item
+        """
+        return item.buying_party == self.criteria
+
+    def calculateSellingPartyFeature(self, item):
+        """ Calculate the selling party feature of a given item.
+
+        Args:
+            item (Derivative): The derivative to get the feature for
+
+        Returns:
+            Boolean: The value of the feature for the given item
+        """
+        return item.selling_party == self.criteria
+
+    def calculateAssetFeature(self, item):
+        """ Calculate the asset party feature of a given item.
+
+        Args:
+            item (Derivative): The derivative to get the feature for
+
+        Returns:
+            Boolean: The value of the feature for the given item
+        """
+        return item.asset == self.criteria
+
+    def calculateQuantityFeature(self, item, mean=0, standardDeviation=0):
+        """ Calculate the quantity feature of a given item.
+
+        Args:
+            item (Derivative): The derivative to get the feature for
+            mean (Integer): The mean to compare ``item`` to
+            standardDeviation (Integer): The standard deviation to compare ``item`` to
+
+        Returns:
+            Boolean: The value of the feature for the given item
+        """
+        result = False
+        if self.criteria == 'less_than_mean':
+            result = item.quantity < mean
+        elif self.criteria == 'more_than_mean':
+            result = item.quantity > mean
+        elif self.criteria == '0_to_1_std':
+            result = abs(mean - item.quantity) <= standardDeviation
+        elif self.criteria == '1_to_2_std':
+            deviation = abs(mean - item.quantity)
+            result = standardDeviation < deviation <= 2 * standardDeviation
+        elif self.criteria == '2_to_3_std':
+            deviation = abs(mean - item.quantity)
+            result = 2 * standardDeviation < deviation <= 3 * standardDeviation
+        elif self.criteria == '3_to_inf_std':
+            deviation = abs(mean - item.quantity)
+            result = deviation > 3 * standardDeviation
+        return result
+
+    def calculateStrikePriceFeature(self, item, mean=0, standardDeviation=0):
+        """ Calculate the strike price feature of a given item.
+
+        Args:
+            item (Derivative): The derivative to get the feature for
+            mean (Integer): The mean to compare ``item`` to
+            standardDeviation (Integer): The standard deviation to compare ``item`` to
+
+        Returns:
+            Boolean: The value of the feature for the given item
+        """
+        result = False
+        if self.criteria == 'less_than_mean':
+            result = item.strike_price < mean
+        elif self.criteria == 'more_than_mean':
+            result = item.strike_price > mean
+        elif self.criteria == '0_to_1_std':
+            result = abs(mean - item.strike_price) <= standardDeviation
+        elif self.criteria == '1_to_2_std':
+            deviation = abs(mean - item.strike_price)
+            result = standardDeviation < deviation <= 2 * standardDeviation
+        elif self.criteria == '2_to_3_std':
+            deviation = abs(mean - item.strike_price)
+            result = 2 * standardDeviation < deviation <= 3 * standardDeviation
+        elif self.criteria == '3_to_inf_std':
+            deviation = abs(mean - item.strike_price)
+            result = deviation > 3 * standardDeviation
+        return result
+
+    def split(self, data):
+        """ Split given data on this node.
+
+        Args:
+            data (List of Derivative): The data to split
+
+        Returns:
+            (List of Derivative, List of Derivative): The split data. The first
+            item is the data that matches this node's feature, the second item
+            is the remaining data
+        """
+        trueSplit = []
+        falseSplit = []
+        if self.feature == Features.BUYING_PARTY:
+            trueSplit = filter(self.calculateBuyingPartyFeature, data)
+            falseSplit = filter(lambda x: not self.calculateBuyingPartyFeature(x), data)
+        elif self.feature == Features.SELLING_PARTY:
+            trueSplit = filter(self.calculateSellingPartyFeature, data)
+            falseSplit = filter(lambda x: not self.calculateSellingPartyFeature(x), data)
+        elif self.feature == Features.SELLING_PARTY:
+            trueSplit = filter(self.calculateSellingPartyFeature, data)
+            falseSplit = filter(lambda x: not self.calculateSellingPartyFeature(x), data)
+        elif self.feature == Features.ASSET:
+            trueSplit = filter(self.calculateAssetFeature, data)
+            falseSplit = filter(lambda x: not self.calculateAssetFeature(x), data)
+        elif self.feature == Features.QUANTITY:
+            mean = 0
+            standardDeviation = 0
+            if self.criteria == 'less_than_mean' or self.criteria == 'more_than_mean':
+                mean = statistics.mean([x.quantity for x in data])
+            else:
+                standardDeviation = statistics.stdev([x.quantity for x in data])
+            trueSplit = filter(lambda x: self.calculateQuantityFeature(x, mean, standardDeviation), data)
+            falseSplit = filter(lambda x: not self.calculateQuantityFeature(x, mean, standardDeviation), data)
+        elif self.feature == Features.STRIKE_PRICE:
+            mean = 0
+            standardDeviation = 0
+            if self.criteria == 'less_than_mean' or self.criteria == 'more_than_mean':
+                mean = statistics.mean([x.strike_price for x in data])
+            else:
+                standardDeviation = statistics.stdev([x.strike_price for x in data])
+            trueSplit = filter(lambda x: self.calculateStrikePriceFeature(x, mean, standardDeviation), data)
+            falseSplit = filter(lambda x: not self.calculateStrikePriceFeature(x, mean, standardDeviation), data)
+
+        return {
+            'true': list(trueSplit),
+            'false': list(falseSplit)
+        }
+
+    def __str__(self):
+        return f'<DecisionTreeNode : {self.id}>'
