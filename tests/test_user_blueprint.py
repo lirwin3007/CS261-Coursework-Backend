32129b1889d832b153eccf34848edbd68517936d
nharikrishnan22 <nhkrishnan4@gmail.com>
2020-02-25 11:42:32 +0000

    ✅ Updated endpoint in testGetUserWillReturn404

--- tests/test_user_blueprint.py
+++ tests/test_user_blueprint.py
@@ -5,7 +5,7 @@ from backend.db import db
 # Check that trying to look for a non-existent user will result in a 404 error
 def testGetUserWillReturn404(test_client, free_user_id):
     # Form endpoint path
-    endpoint = '/get-user/{}'.format(free_user_id)
+    endpoint = '/user-account-control/get-user/{}'.format(free_user_id)
     # Make request and retrieve response
     response = test_client.get(endpoint)
     # Assert that a 404 HTTP error is returned
