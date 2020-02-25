07051f1949408b28225d30d97af9ae6efc0caa85
Neeraj Hari Krishnan <nhkrishnan4@gmail.com>
2020-02-25 18:10:34 +0000

    ✅ Add testGetAction

--- tests/test_action_blueprint.py
+++ tests/test_action_blueprint.py
@@ -3,17 +3,17 @@ from backend.db import db
 
 def testGetAction(test_client, dummy_action):
     # Add dummy derivative to session
-    db.session.add(dummy_derivative)
+    db.session.add(dummy_action)
     db.session.flush()
 
     # Make request and retrieve response
-    url = f'/derivative-management/get-action/{dummy_derivative.id}'
-    response = test_client.get(url)
+    endpoint = f'/action-management/get-action/{dummy_action.id}'
+    response = test_client.get(endpoint)
 
-    # Assert that the response status is 200 OK
+    # Assert that the response status is 200
     assert response.status_code == 200
 
-    # Assert that the response body is the serialization of the derivative
+    # Assert that the response body is the serialization of the action
     assert response.is_json
-    expected_response = jsonify(derivative=dummy_derivative)
+    expected_response = jsonify(action=dummy_action)
     assert response.get_json() == expected_response.get_json()
