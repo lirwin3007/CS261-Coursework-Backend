cfd6d70d9e84488f8a002d9d3184dda3b2cd1159
JDJFisher <jordanfisher2096@gmail.com>
2020-02-28 22:36:20 +0000

    ✅ Readded action tests

---
+++ tests/test_action_blueprint.py
@@ -0,0 +1,23 @@
+# Third party imports
+from flask import jsonify
+
+# Local application imports
+from backend.db import db
+
+
+def testGetAction(test_client, dummy_action):
+    # Add dummy action to session
+    db.session.add(dummy_action)
+    db.session.flush()
+
+    # Make request and retrieve response
+    endpoint = f'/action-management/get-action/{dummy_action.id}'
+    response = test_client.get(endpoint)
+
+    # Assert that the response status is 200
+    assert response.status_code == 200
+
+    # Assert that the response body is the serialization of the action
+    assert response.is_json
+    expected_response = jsonify(action=dummy_action)
+    assert response.get_json() == expected_response.get_json()
